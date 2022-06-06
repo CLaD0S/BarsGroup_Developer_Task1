217f51d753debf39385a3f0fd57269a1000c1279
Егор Кочетаев Александрович <61804964+CLaD0S@users.noreply.github.com>
2022-06-06 05:52:11 +0500

    Update ParallelFlow.cs

--- BarsGroup_Developer_Task1/ParallelFlow.cs
+++ BarsGroup_Developer_Task1/ParallelFlow.cs
@@ -6,19 +6,15 @@
         {
             for (int i = 0; i < countRead; i++)
             {
-                //Console.WriteLine("Читатель " + Thread.CurrentThread.Name + " читает " + Server.GetCount());
                 Server.GetCount();
             }
-            //Console.WriteLine("Читатель " + Thread.CurrentThread.Name + " прочел!");
         }
         public static void ParallelAddCount(int countWrite)
         {
             for (int i = 0; i < countWrite; i++)
             {
                 Server.AddToCount();
-                //Console.WriteLine("Писатель " + Thread.CurrentThread.Name + " записал " + Server.GetCount());
             }
-            //Console.WriteLine("Писатель " + Thread.CurrentThread.Name + " закончил!");
         }
     }
 }
